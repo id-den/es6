330567fdefcd4e585a434b6c8e65be64926a9f29
id-den <r.daniljuk@internetdevels.com>
2016-04-20 14:29:54 +0300

    Use es6 via OOP

--- js/mortgage.js
+++ js/mortgage.js
@@ -1,34 +1,34 @@
-export let calculateMonthlyPayment = (principal, years, rate) => {
-    let monthlyRate = 0;
-    if (rate) {
-        monthlyRate = rate / 100 / 12;
-    }
-    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return {
-        principal: principal,
-        years: years,
-        rate: rate,
-        monthlyPayment: monthlyPayment,
-        monthlyRate: monthlyRate
-    };
-};
+export default class Mortgage {
 
-export let calculateAmortization = (principal, years, rate) => {
-    
-    let amortization = [];
-    let { monthlyRate, monthlyPayment } = calculateMonthlyPayment( principal, years, rate );
-    let balance = principal;
-    for (let y = 0; y < years; y++) {
-        let interestY = 0;                                  // Interest payment for year y
-        let principalY = 0;                                 // Principal payment for year y
-        for (let m = 0; m < 12; m++) {
-            let interestM = balance * monthlyRate;          // Interest payment for month m
-            let principalM = monthlyPayment - interestM;    // Principal payment for month m
-            interestY = interestY + interestM;
-            principalY = principalY + principalM;
-            balance = balance - principalM;
-        }
-        amortization.push({ principalY, interestY, balance });
+    constructor( principal, years, rate ) {
+        this.principal = principal;
+        this.years = years;
+        this.rate = rate;
     }
-    return { monthlyPayment, monthlyRate, amortization };
-};
+
+    get monthlyPayment() {
+        let monthlyRate = this.rate / 100 / 12;
+        return this.principal * monthlyRate / ( 1 - ( Math.pow( 1 / ( 1 + monthlyRate ),
+                this.years * 12 )));
+    }
+
+    get amortization() {
+        let monthlyPayment = this.monthlyPayment;
+        let mothlyRate = this.rate / 100 / 12;
+        let balance = this.principal;
+        let amortization = [];
+        for (let y = 0; y < this.years; y++) {
+            let interestY = 0;
+            let principalY = 0;
+            for (let m = 0; m < 12; m++) {
+                let interestM = balance * mothlyRate;
+                let pricipalM = monthlyPayment - interestM;
+                interestY = interestY + interestM;
+                principalY = principalY + pricipalM;
+                balance = balance - pricipalM;
+            }
+            amortization.push({ principalY, interestY, balance });
+        }
+        return amortization;
+    }
+}
